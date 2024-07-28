caf4c670bee29c9b6dd2bf4b7f4d4dd11f83c9ea
Haris <harisalimughal077@gmail.com>
2024-07-28 14:48:24 +0500

    getHumanchoice function done

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ if(num===4){
     console.log('🥌')
     flag=false
            }
-    else if (num==6){
+    else if (num==5){
         console.log('📄')
         flag=false
     }
@@ -24,5 +24,17 @@ if(num===4){
 }
 
 function gethumanchoice(){
+    let sign = prompt("Enter 4 for Rock, 5 for Paper, 8 for Scissor  ");
 
+    if(sign===4){
+        console.log('🥌')
+               }
+        else if (sign==5){
+            console.log('📄')
+        }
+        else if(sign==8){
+            console.log('✂️');
+        }
 }
+
+gethumanchoice();
