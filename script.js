7db451ebb0d84772cc8d3c66658d3ec51cbba8d7
Haris <harisalimughal077@gmail.com>
2024-07-28 20:39:48 +0500

    playRound function Done

--- script.js
+++ script.js
@@ -1,40 +1,57 @@
-function getcomputerchoice(min, max){
-    
-    return Math.random() * (max - min) + min;   
-}
-let flag=true;
-while(flag){
-let number = getcomputerchoice(4,9);
-let num=Math.floor(number)
-if(num===4){
-    console.log('🥌')
-    flag=false
-           }
-    else if (num==5){
-        console.log('📄')
-        flag=false
-    }
-    else if(num==8){
-        console.log('✂️');
-        flag=false
-    }
-    
+let humanScore=0;
+let computerScore=0;
 
+function getComputerChoice(min, max){
+    let choice=Math.random() * (max - min) + min; 
+    let num=Math.floor(choice)
+    return num
 
 }
 
-function gethumanchoice(){
-    let sign = prompt("Enter 4 for Rock, 5 for Paper, 8 for Scissor  ");
 
-    if(sign===4){
-        console.log('🥌')
+
+    
+function getHumanChoice(){
+    let sign = Number(window.prompt("Enter 1 for Rock, 2 for Paper, 3 for Scissor ",""));
+
+    if(sign!=1 && sign!==2 && sign!=3){
+        console.log("You entered invalid value")
                }
-        else if (sign==5){
-            console.log('📄')
-        }
-        else if(sign==8){
-            console.log('✂️');
-        }
+    
+        return sign;
 }
 
-gethumanchoice();
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice === 1 && computerChoice === 2){
+        console.log("You Lose !, Paper beats Rock")
+        computerScore++;
+    }
+
+    else if(humanChoice === 1 && computerChoice === 3){
+        console.log("You Win!, Rock beats scissors ")
+        humanChoice++;
+    }
+
+    else if(humanChoice === 2 && computerChoice === 1){
+        console.log("You Win!, Paper beats Rock ")
+        humanChoice++;
+    }
+
+    else if(humanChoice === 2 && computerChoice === 3){
+        console.log("You Lose!, Scissors beats Paper ")
+        computerScore++;
+    }
+
+    else if(humanChoice === computerChoice) {
+        console.log("It's draw, both members select equal choice")
+    }
+    
+  }
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice(1,4);
+
+  
+  playRound(humanSelection, computerSelection);
+  
+
