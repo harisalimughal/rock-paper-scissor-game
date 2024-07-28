40ac6e71006fd64d4b5f5163a369d0c4c52114a6
Haris <harisalimughal077@gmail.com>
2024-07-28 13:40:15 +0500

    getComputerchoice function updated, emojis added

--- script.js
+++ script.js
@@ -6,9 +6,23 @@ let flag=true;
 while(flag){
 let number = getcomputerchoice(4,9);
 let num=Math.floor(number)
-if(num===4|| num===6 || num===8){
-    console.log(num)
+if(num===4){
+    console.log('🥌')
     flag=false
+           }
+    else if (num==6){
+        console.log('📄')
+        flag=false
+    }
+    else if(num==8){
+        console.log('✂️');
+        flag=false
+    }
+    
+
+
 }
+
+function gethumanchoice(){
 
 }
